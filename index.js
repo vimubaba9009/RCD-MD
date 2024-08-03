9928ce54daca7ef2c1663cf5d4bb365571c061cc
⚔ DEXTER ⚔ <143838400+DEXTER-BOTS@users.noreply.github.com>
2024-08-03 11:36:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bot = require(__dirname + '/lib/amd')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
-    Debug.info(`Starting wasi ${VERSION}`)
+    Debug.info(`Starting Asta ${VERSION}`)
   try {
     await bot.init()
     //bot.logger.info('⏳ Database syncing!')
@@ -14,9 +14,3 @@ const start = async () => {
   }
 }
 start();
-/**
-
-░▒▓                                                      
-
-                                                      
-**/
